8b0b6f0996640ec0bec1c9c0e8d79b0134e28d1f
Nathaniel96 <lawalolusegun96@gmail.com>
2023-09-07 19:02:26 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+/**
+* main - this is my main
+*
+* Return: this should return zro when code successful
+*/
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
