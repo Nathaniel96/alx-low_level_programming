358ca386dfea4382a0fe14f4d1b46b646b2e1602
Nathaniel96 <lawalolusegun96@gmail.com>
2023-09-07 17:58:20 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - this is the main function
  * Return: this is 0
- */
+*/
 
 int main(void)
 {
    puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+   return(0);
 }
