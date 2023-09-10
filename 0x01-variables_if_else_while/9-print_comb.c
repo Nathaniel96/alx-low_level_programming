f3e813022975713226ef2a35dfaf0aba8cf847b6
Nathaniel96 <lawalolusegun96@gmail.com>
2023-09-10 05:43:11 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,20 +3,19 @@
 /**
  * main - main function
  *
- * Return: always 0 (suuccessful)
+ * Return: always 0 (successful)
  */
 
 int main(void)
 {
 	int i;
-	char j;
 
 	for (i = 0 ; i < 10 ; i++)
-		putchar(i + '0');
-	for (j = 'a' ; j <= 'f' ; j++)
-		putchar(j);
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
 	return (0);
-
-
 }
