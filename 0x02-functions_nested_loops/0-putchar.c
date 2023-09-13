0b56c9ff4e8772098e9cf5366aea812c8dfbf40c
Nathaniel96 <lawalolusegun96@gmail.com>
2023-09-12 17:40:32 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - main function listed _putchar
+ * Return: 0 at end
+ */
+
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
