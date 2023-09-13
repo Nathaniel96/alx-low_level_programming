3cb5a947f38f426246dc4e1ffe47132d43886333
Nathaniel96 <lawalolusegun96@gmail.com>
2023-09-13 06:15:11 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet lowercase 10x
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; j++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+		_putchar("\n");
+	}
+}
