33fac989d55be05b9179792477893cce4c3ee6d4
Nathaniel96 <lawalolusegun96@gmail.com>
2023-09-18 17:26:48 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - int as parameter and updates the value it points to to 98
+ * @n: parameter update.
+ * Return: void.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
