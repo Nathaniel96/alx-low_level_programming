e3d7ab72e2ba793cc127ce39bc172418db76084c
Nathaniel96 <lawalolusegun96@gmail.com>
2023-09-21 14:08:49 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,9 +13,9 @@ char *_strcat(char *dest, char *src)
 	int srclength = 0;
 	int i;
 
-	for (i = 0 ; destlength != '\0' ; i++)
+	for (i = 0 ; dest[i] != '\0' ; i++)
 		destlength++;
-	for (i = 0 ; srclength != '\0' ; i++)
+	for (i = 0 ; src[i] != '\0' ; i++)
 		srclength++;
 	for (i = 0 ; i <= srclength ; i++)
 		dest[destlength + i] = src[i];
