f67e45687497a05d8ec5b5a4df156b58887640ba
Nathaniel96 <lawalolusegun96@gmail.com>
2023-09-21 19:03:09 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * cap_string - Prepare for the worst. Capitalize on what comes
+ * @n: pointer
+ * Return: 0
+ */
+
+char *cap_string(char *n)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (
+	}
+}
