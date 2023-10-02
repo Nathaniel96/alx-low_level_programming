a88e9e160b2c4d27cf8936ac511bff69a1b5fa8c
Nathaniel96 <lawalolusegun96@gmail.com>
2023-10-01 17:20:59 -0700

    6. Inception. Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+/**
+ * is_prime_number -  function that returns 1 if the input integer
+ *  is a prime number, otherwise return 0.
+ *  @x: inpute number
+ *  @y: divisor
+ *  Return: 1 for prime or 0 for composite.
+ */
+
+int prime_a(int x, int y)
+{
+	if (x <= 1 || (x != y && x % y == 0))
+	{
+		return (0);
+	}
+	else if (x == y)
+	{
+		return (1);
+	}
+	return (prime_a(x, y + 1));
+}
+
+/**
+ * is_prime_number -  if the input integer is a prime number
+ * @n: input number
+ * Return: 1 for prime or 0 for composite.
+ */
+
+int is_prime_number(int n)
+{
+	return (prime_a(n, 2));
+}
