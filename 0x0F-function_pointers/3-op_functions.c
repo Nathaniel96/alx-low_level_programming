1e9e7394092f1f02d6fcc8b4fb8def7819c8c45b
Nathaniel96 <lawalolusegun96@gmail.com>
2023-10-12 17:25:36 -0700

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -14,7 +14,6 @@ int op_mod(int a, int b);
  *
  * Return: The sum of a and b.
  */
-	
 int op_add(int a, int b)
 {
 	return (a + b);
@@ -27,12 +26,10 @@ int op_add(int a, int b)
  *
  * Return: The difference of a and b.
  */
-	
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
  * op_mul - Returns the product of two numbers.
  * @a: The first number.
@@ -40,12 +37,10 @@ int op_sub(int a, int b)
  *
  * Return: The product of a and b.
  */
-	
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-	
 /**
  * op_div - Returns the division of two numbers.
  * @a: The first number.
@@ -53,12 +48,10 @@ int op_mul(int a, int b)
  *
  * Return: The quotient of a and b.
  */
-	
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-	
 /**
  * op_mod - Returns the remainder of the division of two numbers.
  * @a: The first number.
@@ -66,7 +59,6 @@ int op_div(int a, int b)
  *
  * Return: The remainder of the division of a by b.
  */
-	
 int op_mod(int a, int b)
 {
 	return (a % b);
